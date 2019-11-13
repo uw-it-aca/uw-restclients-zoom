264f2b7084b35005102a52b2caa5b8b8cff2a6cf
Jim Laney <jlaney@uw.edu>
2019-11-13 13:46:34 -0800

    add pyjwt and authn header

--- setup.py
+++ setup.py
@@ -23,9 +23,10 @@ setup(
     author_email="aca-it@uw.edu",
     include_package_data=True,
     install_requires=['UW-RestClients-Core>1.1.1,<2.0',
-                      'mock',
+                      'PyJWT',
                       'python-dateutil',
                       'pytz',
+                      'mock',
                      ],
     license='Apache License, Version 2.0',
     description=('A library for connecting to Zoom at the University '
--- uw_zoom/dao.py
+++ uw_zoom/dao.py
@@ -2,7 +2,10 @@
 Contains UW Zoom DAO implementations.
 """
 from restclients_core.dao import DAO
+from commonconf import settings
 from os.path import abspath, dirname
+from time import time
+import jwt
 import os
 
 
@@ -11,5 +14,17 @@ class ZOOM_DAO(DAO):
         return 'zoom'
 
     def service_mock_paths(self):
-        path = [abspath(os.path.join(dirname(__file__), "resources"))]
-        return path
+        return [abspath(os.path.join(dirname(__file__), 'resources'))]
+
+    def _generate_jwt(self):
+        headers = {'alg': 'HS256', 'typ': 'JWT'}
+        payload = {
+            'iss': self.get_service_setting('API_KEY', ''),
+            'exp': int(time() + self.get_service_setting('VALID_FOR', 10)),
+        }
+        secret = self.get_service_setting('API_SECRET', '')
+        return jwt.encode(payload, secret, algorithm='HS256', headers=headers)
+
+    def _custom_headers(self, method, url, headers, body):
+        token = self._generate_jwt()
+        return {'Authorization': 'Bearer {}'.format(token.decode())}
