6a5e72cde80e396fa750069e840e0210bffd4bdd
Jim Laney <jlaney@uw.edu>
2021-05-11 11:37:12 -0700

    add license/copyright to src files

--- uw_zoom/tests/test_accounts.py
+++ uw_zoom/tests/test_accounts.py
@@ -1,3 +1,6 @@
+# Copyright 2021 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
 from unittest import TestCase
 from uw_zoom.utilities import fdao_zoom_override
 from uw_zoom.models import ZoomAccount, ZoomUser
